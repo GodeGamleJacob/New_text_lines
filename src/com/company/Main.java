c45b66e55a40720025d3015f4ecec16f3b3eb38f
GodeGamleJacob <89967283+GodeGamleJacob@users.noreply.github.com>
2021-09-02 21:33:44 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Reading homework is \"fun\" when it's 9PM.");
+        System.out.print("Although!\nI could have just started sooner, rather than later right?\n");
+    }
+}
